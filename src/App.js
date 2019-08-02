43ec075aadde1a71eb65a83108cace60f7048c1f
Eviatar <eviatar@MacBook-Pro-Mars.local>
2019-08-02 13:12:03 +0300

    addedlogo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navigation></Navigation>
+      <Logo></Logo>
     </div>
   );
 }
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+import './Logo.css';
+import Eyecon from './eye-con.png';
+
+
+const Logo = () => {
+    return (
+        <div className='ma4 mt0'>
+            <Tilt 
+            className="Tilt br2 shadow-2" 
+            options={{ 
+                max: 55 
+            }} 
+            style={{  width: 200 }} >
+                <div className="Tilt-inner pa3 f3 fw5 lh-copy"> Recogn
+                <img style={{height:'1.5rem',marginBottom:'-5px'}} src={Eyecon} alt=''></img>
+                 y
+                 <img style={{height:'1.5rem',marginBottom:'-5px'}}  src={Eyecon} alt=''></img>
+                  s</div>
+            </Tilt>
+
+        </div>
+
+    )
+}
+
+export default Logo;
