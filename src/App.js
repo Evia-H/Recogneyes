f9df6b9719c42b65420f1b2954d987d1c707159f
Eviatar <eviatar@MacBook-Pro-Mars.local>
2019-08-02 14:23:14 +0300

    added rank

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+
+
 
 import './App.css';
 
@@ -9,6 +13,8 @@ function App() {
     <div className="App">
       <Navigation></Navigation>
       <Logo></Logo>
+      <Rank></Rank>
+      <ImageLinkForm></ImageLinkForm>
     </div>
   );
 }
