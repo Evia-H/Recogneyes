051f41d57b78e65e6f0ffd7a9edcdb280057e64b
Evia-H <evi1020@gmail.com>
2019-08-04 23:47:02 +0300

    test commit

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ class App extends Component {
           console.log(response)
         },
         function(err) {
-          // there was an error
+          console.log(err)
         }
       );
     }
